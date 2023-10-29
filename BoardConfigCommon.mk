b8111759034497c9114888df199a3a2bdcdc9ae8
dotkit <ewprjkt@proton.me>
2023-10-29 06:17:54 +0000

    sdm660-common: configs: Add `device_framework_compatibility_matrix`
    Change-Id: I366b5e55e7610258d76ab3e2d4802322df09361a
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/manifests/device_framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
